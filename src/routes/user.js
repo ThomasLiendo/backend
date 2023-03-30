e082c74897396f1b1e412db36d09a913015912f8
profesorjand <profesorjand@gmail.com>
2023-03-30 19:24:50 -0300

    delete User y ajustes en modelos del usuario

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,27 +2,25 @@ const { Router } = require('express');
 const allUsers = require('../middlewares/users/allUsers');
 const createUser = require('../middlewares/users/createUser');
 const updateUsers = require('../middlewares/users/updateUsers');
+const deleteUser = require('../middlewares/users/deleteUser');
 
 const router = Router();
 
-router.get('/',allUsers, async(req,res)=>{
-
+router.get('/', allUsers, async(req,res)=>{
     return res.json(req.body.allUsers) 
 })
 
 router.post('/', createUser, async(req,res)=>{
     const {nombre, apellido, email} = req.body;
-    return res.status(200).send({respuesta:`el Usuario ` + nombre + ` ` + apellido + ` con email ` + email + ` se ah creado exitosamente`}) 
+    return res.status(200).send(req.body.resultado) 
 })
 
-router.put('/',updateUsers, async(req,res)=>{
-    
+router.put('/', updateUsers, async(req,res)=>{   
     return res.json({respuesta: `usuario actualizado`}) 
 })
 
-// router.delete('/',allUsers, async(req,res)=>{
-    
-//     return res.json(req.body.allUsers) 
-// })
+router.delete('/', deleteUser, async(req,res)=>{   
+    return res.json({respuesta:`usuario con id ${req.body.eliminado} eliminado`}) 
+})
 
 module.exports = router;
