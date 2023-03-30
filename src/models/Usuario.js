0e9c7acd74bebce69eed7ae16a2fd056ddfad0dc
profesorjand <profesorjand@gmail.com>
2023-03-30 19:24:50 -0300

    delete User y ajustes en modelos del usuario

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -15,19 +15,41 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
+      validate:{
+        isAlpha: {
+          msg: "El Nombre debe ser solo Texto"
+        }
+      }
     },
     apellido: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate:{
+        isAlpha: {
+          msg: "El Apellido debe ser solo Texto"
+        }
+      }
+      
     },
     email: {
       type: DataTypes.STRING,
       unique: true,
       allowNull: false,
+      validate:{
+        isEmail: {
+          msg: "El Formato del Email no es el correcto"
+        }
+      }
     },
     clave: { 
       type: DataTypes.STRING,
       allowNull: false,
+      validate:{
+        len: [5,20], 
+        isInt: {
+          msg: "La Clave debe ser entre 5 a 20 caracteres"
+        }
+      }
     },
   },);
 };
