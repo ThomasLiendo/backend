8cc027913ca1a901aa162cc3f35b63ea49f3411a
profesorjand <profesorjand@gmail.com>
2023-03-30 17:39:50 -0300

    update User Put

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const allUsers = require('../middlewares/users/allUsers');
 const createUser = require('../middlewares/users/createUser');
+const updateUsers = require('../middlewares/users/updateUsers');
 
 const router = Router();
 
@@ -14,10 +15,10 @@ router.post('/', createUser, async(req,res)=>{
     return res.status(200).send({respuesta:`el Usuario ` + nombre + ` ` + apellido + ` con email ` + email + ` se ah creado exitosamente`}) 
 })
 
-// router.put('/',allUsers, async(req,res)=>{
+router.put('/',updateUsers, async(req,res)=>{
     
-//     return res.json(req.body.allUsers) 
-// })
+    return res.json({respuesta: `usuario actualizado`}) 
+})
 
 // router.delete('/',allUsers, async(req,res)=>{
     
