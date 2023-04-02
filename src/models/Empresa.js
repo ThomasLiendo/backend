980490baad7cf5238613825699260e0e5881831b
ThomasLiendo <thomasliendo00@gmail.com>
2023-04-02 16:51:35 -0300

    modelo de la Empresa

---
+++ src/models/Empresa.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("Empresa", {
+    //id se crea automatico
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      unique: true,
+      allowNull: false,
+    },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    descripcion: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
