856dd71915ed19cea9d27aa444ff3164d6ec3149
dileo24 <joacodileo@gmail.com>
2023-04-04 00:04:00 -0300

    info => descr

--- src/models/Producto.js
+++ src/models/Producto.js
@@ -25,7 +25,7 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: true,
     },
-    informacion: {
+    descripcion: {
       type: DataTypes.STRING,
       allowNull: true,
     },
