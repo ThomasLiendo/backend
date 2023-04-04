1455a7611cd159574c2f2daf08c08be790170ef4
dileo24 <joacodileo@gmail.com>
2023-04-03 22:22:26 -0300

    union con el front

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,9 @@ const express = require("express");
 // Ejemplo: const authRouter = require('./auth.js');
 
 const Categories = require("./categories");
-const Home = require('./home');
-const User = require('./user');
-const Product = require('./producto')
-
+const Home = require("./home");
+const User = require("./user");
+const Product = require("./producto");
 
 const router = express();
 
@@ -16,11 +15,10 @@ router.use(express.json());
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-router.use("/categories", Categories);
-router.use('/product', Product );
-router.use('/user', User );
-router.use('/', Home );
-
+router.use("/categorias", Categories);
+router.use("/productos", Product);
+router.use("/usuarios", User);
+router.use("/", Home);
 
 router.all("*", (req, res) => {
   res.redirect("/");
