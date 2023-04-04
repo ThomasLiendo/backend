6bad884d50bea38228778690db937825d9b16d3f
profesorjand <profesorjand@gmail.com>
2023-04-04 02:50:53 -0300

    Update de Rutas de products añadir codigo

--- src/middlewares/products/createProducts.js
+++ src/middlewares/products/createProducts.js
@@ -2,14 +2,15 @@ const { Producto, Op } = require("../../db");
 
 const createProduct = async (req, res, next) => {
     try{
-        const {nombre, descripcion} = req.body;
+        const {nombre, descripcion, codigo} = req.body;
         console.log(nombre)
         if(typeof nombre !== "string" || nombre === undefined){
             throw new Error(`El Nombre del Producto debe ser unicamente texto, y has insertado ${nombre === undefined ? "texto vacio": nombre}`)
         } 
         const newProduct = await Producto.create({
             nombre,
-            descripcion
+            descripcion,
+            codigo
         })
         req.body.resultado = {status:"200", respuesta:`el Producto ${nombre} se ah creado exitosamente`};
         next();
