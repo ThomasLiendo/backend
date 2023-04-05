b14611a5c6b739145445840aaef61910f12951fe
ThomasLiendo <thomasliendo00@gmail.com>
2023-04-04 22:42:00 -0300

    se implemento la busqueda por id de usuarios y depositos

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,25 +1,31 @@
-const { Router } = require('express');
-const allUsers = require('../middlewares/users/allUsers');
-const createUser = require('../middlewares/users/createUser');
-const updateUsers = require('../middlewares/users/updateUsers');
-const deleteUser = require('../middlewares/users/deleteUser');
+const { Router } = require("express");
+const allUsers = require("../middlewares/users/allUsers");
+const createUser = require("../middlewares/users/createUser");
+const updateUsers = require("../middlewares/users/updateUsers");
+const deleteUser = require("../middlewares/users/deleteUser");
+const findUserByID = require("../middlewares/users/findUserByID");
 
 const router = Router();
 
-router.get('/', allUsers, async(req,res)=>{
-    return res.json(req.body.allUsers) 
-})
+router.get("/", allUsers, async (req, res) => {
+  return res.json(req.body.allUsers);
+});
 
-router.post('/', createUser, async(req,res)=>{
-    return res.status(200).send(req.body.resultado) 
-})
+router.post("/", createUser, async (req, res) => {
+  return res.status(200).send(req.body.resultado);
+});
+router.get("/:id", findUserByID, async (req, res) => {
+  return res.json(req.body.findUserByID);
+});
 
-router.put('/:id', updateUsers, async(req,res)=>{   
-    return res.json(req.body.resultado) 
-})
+router.put("/:id", updateUsers, async (req, res) => {
+  return res.json(req.body.resultado);
+});
 
-router.delete('/:id', deleteUser, async(req,res)=>{   
-    return res.json({respuesta:`usuario con id ${req.body.eliminado} eliminado`}) 
-})
+router.delete("/:id", deleteUser, async (req, res) => {
+  return res.json({
+    respuesta: `usuario con id ${req.body.eliminado} eliminado`,
+  });
+});
 
 module.exports = router;
