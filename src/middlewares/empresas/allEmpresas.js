b57d31d6a541c3d649cca590d803ef39e44c0835
profesorjand <profesorjand@gmail.com>
2023-04-05 01:19:30 -0300

    añadir Rutas de Empresas y arreglos de Rols y Cat

---
+++ src/middlewares/empresas/allEmpresas.js
@@ -0,0 +1,27 @@
+const {
+  Empresa,
+  Usuario,
+  Deposito,
+  Producto,
+  Categoria,
+  Subcategoria,
+  Op,
+} = require("../../db");
+
+const allEmpresa = async (req, res, next) => {
+  try {
+    req.body.AllEmpresas = await Empresa.findAll({
+      include: Usuario,
+      Deposito,
+      Producto,
+      Categoria,
+      Subcategoria,
+    });
+    next();
+  } catch (err) {
+    console.log("error en allEmpresa");
+    res.status(404);
+  }
+};
+
+module.exports = allEmpresa;
