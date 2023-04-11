751a0e5fa2dd8894bd86795368ea9bdc546fe7e4
dileo24 <joacodileo@gmail.com>
2023-04-10 23:22:18 -0300

    text en depositos description

--- src/models/Deposito.js
+++ src/models/Deposito.js
@@ -37,7 +37,7 @@ module.exports = (sequelize) => {
       allowNull: false,
     },
     descripcion: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
       allowNull: false,
     },
   });
